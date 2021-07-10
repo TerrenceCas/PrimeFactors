f4acf3191806468b292b23a1c764e8f684d27631
Terrence C <mc.terrence.castrence@gmail.com>
2021-07-10 16:27:07 -0500

    Add project files.

---
+++ PrimeFactors/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeFactors
+{
+    public class Program
+    {
+        public static string primeFactors(int number)
+        {
+            var primes = new List<int>();
+
+            for (int div = 2; div <= number; div++)
+            {
+                while (number % div == 0)
+                {
+                    primes.Add(div);
+                    number /= div;
+                }
+            }
+
+            return string.Join(" x ", primes);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Prime factors of {input} are: {primeFactors(input)}"); 
+            // 40 = 2 x 2
+            // 7  = 7
+            // 30 = 2 x 3 x 5
+            // 40 = 2 x 2 x 2 x 5
+            // 50 = 2 x 5 x 5
+        }
+    }
+}
---
+++ ProgramPrimeFactorsUT/UnitTest1.cs
@@ -0,0 +1,52 @@
+using NUnit.Framework;
+using PrimeFactors;
+namespace ProgramPrimeFactorsUT
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Prime_factors_of_4()
+        {
+            string actualResult = Program.primeFactors(4);
+            string expectedResult = "2 x 2";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Prime_factors_of_7()
+        {
+            string actualResult = Program.primeFactors(7);
+            string expectedResult = "7";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Prime_factors_of_30()
+        {
+            string actualResult = Program.primeFactors(30);
+            string expectedResult = "2 x 3 x 5";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Prime_factors_of_40()
+        {
+            string actualResult = Program.primeFactors(40);
+            string expectedResult = "2 x 2 x 2 x 5";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Prime_factors_of_50()
+        {
+            string actualResult = Program.primeFactors(50);
+            string expectedResult = "2 x 5 x 5";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
